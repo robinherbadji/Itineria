65167fa1e38fcf06b9d51cc4ad235b0ec54b8576
robinherbadji <robin.herbadji@gmail.com>
2020-03-25 01:57:52 +0100

    Debut ParcoursEnLargeur

--- src/fr/uha/ensisa/itineria/moteur/algorithmes/ParcoursEnLargeur.java
+++ src/fr/uha/ensisa/itineria/moteur/algorithmes/ParcoursEnLargeur.java
@@ -1,8 +1,14 @@
 package fr.uha.ensisa.itineria.moteur.algorithmes;
 
 
+import java.util.ArrayList;
+
+import fr.uha.ensisa.itineria.donnees.ArbreDeRecherche;
 import fr.uha.ensisa.itineria.donnees.Carte;
+import fr.uha.ensisa.itineria.donnees.Noeud;
 import fr.uha.ensisa.itineria.donnees.Parametres;
+import fr.uha.ensisa.itineria.donnees.Route;
+import fr.uha.ensisa.itineria.donnees.Ville;
 
 
 /**
@@ -19,8 +25,24 @@ public class ParcoursEnLargeur extends Algorithme {
 		
 	}
 	
+	/**
+	 * 
+	 * @author Robin
+	 *
+	 */
 	public void launch()
 	{
+		// TODO
+		
+		
+		Ville ville = parametres.getDepart();
+		Noeud racine = new Noeud(ville); 
+		arbre = new ArbreDeRecherche(racine);
+		
+		while(!verifierObjectif(ville)) {
+			ArrayList<Route> routes = ville.getRoutesVersVoisins();
+			// ..
+		}
 		
 	}
 	
